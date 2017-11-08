cc7114222b133fad25f64d3f660adbf8b08e77ee
Kshitij Bantupalli <kshitij.bantupalli@gmail.com>
2017-11-08 15:06:11 -0500

    Changes to server and client
    Running locally

--- client.py
+++ client.py
@@ -1,16 +1,35 @@
-import Polynomials
 import socket
 
 BUFFSIZE = 1024
 print "Starting client prog"
 s = socket.socket()
 host = 'cs3.kennesaw.edu'
-#host = socket.gethostname()
 port = 12321
 
-s.connect((host, port))
-print "Client connected to server: ", host
-print "Sending my name to server ", host
-s.send("Kshitij Bantupalli")
-print s.recv(BUFFSIZE)
+'''
+Variables for data manipulation.
+arg_val =  Argument value for request type E.
+request_type = Type of request to be sent.
+poly = List of numbers.
+a = value a for bisection.
+b = value b for bisection.
+tol = tolerance in bisection.
+'''
+
+request_type = 'E'
+arg_val = 1.0
+poly = [631,232,223,222]
+a = 3
+b = 2
+tol = 0.00000000001
+
+request = [request_type, arg_val,' ', poly]
+#request = [request_type, a, ' ', b, ' ', poly, ' ', tol]
+s.connect(('', port))
+s.send(request)
+recieve = s.recv(BUFFSIZE)
+if (recieve[0] == 'E' || recieve[0] == 'S'):
+    print(recieve)
+else if(recive[0] == 'X')
+    print(recieve)
 s.close
--- server.py
+++ server.py
@@ -1,17 +1,78 @@
+import Polynomials
 import socket
 
 BUFFSIZE = 1024
-print "Starting server prog"
-s = socket.socket()
-#host = socket.gethostname()
+print "Starting server program."
+
+#Create Sockets
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print "Socket successfully created."
+except socket.error as err:
+    print "Socket creation failed with error %s" %(err)
+
+# Define the port to be used.
 port = 12321
+
+'''
+#Get IP of host.
+try:
+    host = socket.gethostname('cs3.kennesaw.edu')
+except socket.gaierror:
+    print "There was an error resolving the host."          #Error control for host
+    sys.exit()
+'''
+
 s.bind(('', port))
 
 s.listen(5)
 while True:
     c, addr = s.accept()
     print 'Got connection from', addr
-    namedata = c.recv(BUFFSIZE)
-    print 'recieved: ', namedata
-    c.send(host + 'Ending connection to: ')
-    c.close()
+    request = c.recv(BUFFSIZE)
+    if request[0] = 'E':
+        if(len(request) < 4 || len(request) > 4):
+            error = ['X', "Missing arguments."]
+            c.send(error)
+            print("Shutting down server.")
+            c.close()
+        else if !(all(isinstance(item, int) for item in request[4])):
+            error = ['X', "Invalid number format."]
+            c.send(error)
+            print("Shutting down server.")
+            c.close()
+        else:
+            x = request[1]
+            poly = request[4]
+            temp = evaluate(x, poly)
+            success = ['E', temp]
+            c.send(success)
+            print ("Server finished. Shutting down.")
+            c.close()
+    else if request[0] = 'X':
+        if(len(request) < 7 || len(request) > 7):
+            error = ['X', "Missing Arguments"]
+            c.send(error)
+            print("Shutting down server.")
+            c.close()
+        else if !(all(isinstance(item, int) for item in request[5])):
+            error = ['X', "Invalid number format."]
+            c.send(error)
+            print("Shutting down server.")
+            c.close()
+        else:
+            a = request[1]
+            b = request[3]
+            poly = request[5]
+            tol = request[7]
+            temp = bisection(a, b, poly, tol)
+            success = ['S', temp]
+            c.send(success)
+            print("Server finished. Shutting down.")
+            c.close()
+    else:
+        temp = "Please enter correct request type."
+        error = ['X', temp]
+        c.send(error)
+        print("Shutting down server.")
+        c.close()
